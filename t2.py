9078ca9020092cd5ff0bd24d020a3d60e425963c
hec1992 <73147559+hec1992@users.noreply.github.com>
2021-04-26 11:57:37 -0400

    Add files via upload

---
+++ t2.py
@@ -0,0 +1,13 @@
+foods = ['straw', 'pork', 'turkey', 'chicken']
+for f in foods:
+    print (f)
+    print (len(f))
+    for x in range (10):
+        print (x)
+
+Hector = 2
+
+while Hector < 25:
+    print (Hector)
+    Hector += 1
+x = 100
---
+++ t3.py
@@ -0,0 +1,3 @@
+inputs = 'Hi'
+print('Good morning all, how was your weekend' + inputs)
+user = input
